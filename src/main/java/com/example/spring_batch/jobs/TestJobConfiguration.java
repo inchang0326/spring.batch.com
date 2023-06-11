75b9624907354d099aa0225c786c4991c9f31c30
steady <inchang0326@naver.com>
2023-06-11 19:08:04 +0900

    Spring Batch Practice

---
+++ src/main/java/com/example/spring_batch/jobs/TestJobConfiguration.java
@@ -0,0 +1,38 @@
+package com.example.spring_batch.jobs;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class TestJobConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean(name = "TESTJOB01")
+    public Job testJob() {
+        return jobBuilderFactory.get("TESTJOB01")
+                .start(testStep())
+                .build();
+    }
+
+    @Bean(name = "TESTJOB01_TESTSTEP02")
+    public Step testStep() {
+        return stepBuilderFactory.get("TESTJOB01_TESTSTEP02")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("step >>>>");
+                    return RepeatStatus.FINISHED;
+                })
+                .allowStartIfComplete(true)
+                .build();
+    }
+}

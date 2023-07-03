7a93640cd9d102ae25426131a2c4f1d8c78ecab6
steady <inchang0326@naver.com>
2023-07-03 23:55:27 +0900

    Test Code

--- src/test/java/com/example/spring_batch/SpringBatchApplicationTests.java
+++ src/test/java/com/example/spring_batch/SpringBatchApplicationTests.java
@@ -1,15 +1,13 @@
 package com.example.spring_batch;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@SpringBatchTest
 class SpringBatchApplicationTests {
 
     @Test
     void contextLoads() {
-    }
 
+    }
 }
--- src/test/java/com/example/spring_batch/jobs/TestJobTest.java
+++ src/test/java/com/example/spring_batch/jobs/TestJobTest.java
@@ -1,7 +1,36 @@
 package com.example.spring_batch.jobs;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@SpringBatchTest
 class TestJobTest {
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
 
+    @Test
+    public void MultiThreadStepWithMyBatisPagingTest() throws Exception {
+
+        // given
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("ver", "2.0")
+                .addString("date", "20230326")
+                .addString("id", "hahahaha")
+                .toJobParameters();
+        // when
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
+
+        // then
+        assertThat(jobExecution.getStatus()).isEqualTo(BatchStatus.COMPLETED);
+    }
 }
